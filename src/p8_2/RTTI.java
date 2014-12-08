5cf05471498cb89c2da5b1b796f03d5e6e3b4b5b
luanzhikun <972663246@qq.com>
2014-12-08 23:11:21 +0800

    项目提交

---
+++ src/p8_2/RTTI.java
@@ -0,0 +1,34 @@
+package p8_2;
+
+public class RTTI 
+{
+	public static void main(String[] args) {
+		Useful[] x =
+			{
+				new Useful(),
+				new MoreUseful()
+			};
+		x[0].f();
+		x[1].g();
+		
+		((MoreUseful)x[1]).u();
+		((MoreUseful)x[0]).u();
+	}
+
+}
+
+
+class Useful
+{
+	public void f() {}
+	public void g() {}
+}
+
+class MoreUseful extends Useful
+{
+	public void f() {}
+	public void g() {}
+	public void u() {}
+	public void v() {}
+	public void w() {}
+}
